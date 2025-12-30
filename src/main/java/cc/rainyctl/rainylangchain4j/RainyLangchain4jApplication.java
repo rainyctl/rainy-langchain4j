5cf4225bf9b794db94dd6cdaa71e851dfd3540e8
rainyctl <rainyctl@gmail.com>
2025-12-29 20:50:40 -0800

    init proj

---
+++ src/main/java/cc/rainyctl/rainylangchain4j/RainyLangchain4jApplication.java
@@ -0,0 +1,13 @@
+package cc.rainyctl.rainylangchain4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainyLangchain4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RainyLangchain4jApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cc/rainyctl/rainylangchain4j/RainyLangchain4jApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.rainyctl.rainylangchain4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainyLangchain4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
